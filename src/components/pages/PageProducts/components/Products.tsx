8d5129e8c1763dd54a2286d2e327bf9a1e2172cc
vitaliimarushko <36469941+vitaliimarushko@users.noreply.github.com>
2023-02-22 19:10:02 +0200

    Adapt source code; upgrade dependencies (#1)

--- src/components/pages/PageProducts/components/Products.tsx
+++ src/components/pages/PageProducts/components/Products.tsx
@@ -17,7 +17,6 @@ export default function Products() {
 
   return (
     <Grid container spacing={4}>
-      {/* eslint-disable-next-line @typescript-eslint/no-unused-vars */}
       {data.map(({ count, ...product }, index) => (
         <Grid item key={product.id} xs={12} sm={6} md={4}>
           <Card
