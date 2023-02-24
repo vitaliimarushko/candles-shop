24e3b7cd44b5cb3502039870851d850e82c346c7
Vitalii Marushko <vi.marushko@gmail.com>
2023-02-24 16:24:08 +0200

    Update real URL in client app

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://ym2rzsu3va.execute-api.eu-central-1.amazonaws.com/dev",
+  bff: "https://9hhpsvckda.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
